20d7475541e8f7cdcf61ae018a5bec081b9dec8a
Max Starr <contact@maxstarr.xyz>
2020-11-26 19:44:31 -0800

    some setup

--- backend/sql/indexes.sql
+++ backend/sql/indexes.sql
@@ -1,1 +1,3 @@
 -- Index Your Tables Here --
+--  from asg 8
+CREATE INDEX mailbox_idx ON mail(mailbox);
--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -3,3 +3,6 @@ DROP TABLE IF EXISTS dummy;
 CREATE TABLE dummy(created TIMESTAMP WITH TIME ZONE);
 
 -- Your database schema goes here --
+-- from asg
+DROP TABLE IF EXISTS mail;
+CREATE TABLE mail(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), mailbox VARCHAR(32), mail jsonb);
